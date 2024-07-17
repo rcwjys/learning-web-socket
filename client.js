aec523262b5600994dcb223a30d2e6a4b16480a9
rcwjys <rcwjys@gmail.com>
2024-07-17 13:21:24 +0700

    test

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io('http://localhost:8182', {
+  auth: {
+    token: '08f42cca93c1b277056b84afd79e1b9349d6169ee55482c6e1ebb24c42409c3b1c63a646468d7f8d49acef3d890d47ee3fc70629a52abe0269f42d58c43fa604'
+  }
+});
 
 
-const user1 = {
-  name: "rico",
-  age: 20,
-}
 
 socket.on('connect', () => {
   console.log(`Connected to server with ID: ${socket.id}`);
